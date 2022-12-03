bf5e42ca47061f130a1c513410360de81c260540
jasoneli <eliyajasona@gmail.com>
2022-12-03 15:42:38 +0000

    master

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import Card from './Card';
-import CardList from './CardList'
+import App from './App';
+//import CardList from './CardList'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import  { robots }  from './robots';
+//import  { robots }  from './robots';
 
 //ReactDOM.render(<Card />,document.getElementById('root'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/> 
+    <App/>
   </React.StrictMode>
 );
 
